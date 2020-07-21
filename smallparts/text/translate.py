14f6329403c7d2e9013be8f618c826a22ebd2053
blackstream-x <rainer@blackstream.de>
2020-07-21 18:37:50 +0200

    removed a YAGNI class

--- smallparts/text/translate.py
+++ smallparts/text/translate.py
@@ -6,52 +6,7 @@ smallparts.text.translate - text translation functions
 
 """
 
-import re
-
 from smallparts import constants
-
-
-class MakeTranslationFunction():
-
-    """Make a function for multiple replacements
-    (adapted from Python Cookbook, Recipe 1.18, see
-     <https://www.oreilly.com/library/view/python-cookbook/0596001673/ch03s15.html>)
-    """
-
-    def __init__(self, *args, **kwargs):
-        """Build a mapping of strings and their replacements,
-        precompile the catch-all regular expression"""
-        self.replacements = dict(*args, **kwargs)
-        self.prx_catch_all = self.precompile_regex()
-
-    def __call__(self, original_text):
-        """Execute the second form of regular expression substitution
-        using a function instead of a string as replacement,
-        see <https://docs.python.org/library/re.html#re.sub>
-        """
-        return self.prx_catch_all.sub(self.single_translation,
-                                      original_text)
-
-    @property
-    def catch_all_pattern(self):
-        """Provide the catch-all regular expression pattern as a property"""
-        return constants.PIPE.join(re.escape(single_pattern)
-                                   for single_pattern
-                                   in self.replacements)
-
-    def precompile_regex(self):
-        """Precompile the catch-all regular expression"""
-        return re.compile(self.catch_all_pattern)
-
-    def single_translation(self, match):
-        """The core function performing the replacement
-        of a single string"""
-        return self.replacements[match.group(constants.ZERO)]
-
-
-#
-# End of classes, start of functions
-#
 
 
 def remove_trailing_underscores(name):
