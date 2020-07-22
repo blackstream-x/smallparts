69b36616305f5048c1ed26965b1708382cd0a55c
blackstream-x <rainer@blackstream.de>
2020-07-22 12:25:00 +0200

    Add a generic remove= parameter to the defuse() function (closes #18)

--- smallparts/markup/characters.py
+++ smallparts/markup/characters.py
@@ -36,27 +36,33 @@ PRX_NUMERIC_ENTITY = re.compile(
 # <https://www.w3.org/TR/xml11/Overview.html#charsets>
 # for XML 1.1
 
+INVALID = 'invalid'
+RESTRICTED = 'restricted'
+DISCOURAGED = 'discouraged'
+
+SUPPORTED_CHARACTER_REMOVALS = (INVALID, RESTRICTED, DISCOURAGED)
+
 SUPPORTED_XML_VERSIONS = (constants.XML_1_0, constants.XML_1_1)
 
 RESTRICTED_C0_CONTROLS = \
-    list(range(0x1, 0x9)) + [0xb, 0xc] + list(range(0xe, 0x20))
+    tuple(range(0x1, 0x9)) + (0xb, 0xc) + tuple(range(0xe, 0x20))
 RESTRICTED__IN_XML_1_0 = \
-    list(range(0x7f, 0x85)) + list(range(0x86, 0xa0))
+    tuple(range(0x7f, 0x85)) + tuple(range(0x86, 0xa0))
 INVALID_IN_XML_1_1 = \
-    [0x0] + list(range(0xd800, 0xe000)) + [0xfffe, 0xffff]
+    (0x0,) + tuple(range(0xd800, 0xe000)) + (0xfffe, 0xffff)
 INVALID_CODEPOINTS = {
     constants.XML_1_0: INVALID_IN_XML_1_1 + RESTRICTED_C0_CONTROLS,
     constants.XML_1_1: INVALID_IN_XML_1_1}
 RESTRICTED_CODEPOINTS = {
     constants.XML_1_0: RESTRICTED__IN_XML_1_0,
     constants.XML_1_1: RESTRICTED__IN_XML_1_0 + RESTRICTED_C0_CONTROLS}
-DISCOURAGED_CODEPOINTS = list(range(0xfdd0, 0xfde0)) + [
+DISCOURAGED_CODEPOINTS = tuple(range(0xfdd0, 0xfde0)) + (
     0x1fffe, 0x1ffff, 0x2fffe, 0x2ffff, 0x3fffe, 0x3ffff,
     0x4fffe, 0x4ffff, 0x5fffe, 0x5ffff, 0x6fffe, 0x6ffff,
     0x7fffe, 0x7ffff, 0x8fffe, 0x8ffff, 0x9fffe, 0x9ffff,
     0xafffe, 0xaffff, 0xbfffe, 0xbffff, 0xcfffe, 0xcffff,
     0xdfffe, 0xdffff, 0xefffe, 0xeffff, 0xffffe, 0xfffff,
-    0x10fffe, 0x10ffff]
+    0x10fffe, 0x10ffff)
 
 #
 #
@@ -145,32 +151,40 @@ def encode_to_charrefs(source_text):
 
 def defuse(source_text,
            target_xml_version=constants.XML_1_0,
-           remove_restricted=False,
-           remove_discouraged=False):
+           remove=INVALID):
     """Defuse source_text for use as content of an XML element:
     Remove invalid codepoints and, if requested,
     also restricted and/or discouraged ones
     (removing discouraged codepoints implies removing restricted ones).
     Escape special characters (<, > and &).
     """
-    try:
-        remove_codepoints = dict.fromkeys(
-            INVALID_CODEPOINTS[target_xml_version])
-    except KeyError:
+    if target_xml_version not in SUPPORTED_XML_VERSIONS:
         raise ValueError(
             'target_xml_version must be one of {0!r}!'.format(
                 SUPPORTED_XML_VERSIONS))
     #
-    if remove_restricted or remove_restricted:
-        remove_codepoints.update(
-            dict.fromkeys(RESTRICTED_CODEPOINTS[target_xml_version]))
+    if remove is None:
+        cleaned_up_source = source_text
+    elif remove in SUPPORTED_CHARACTER_REMOVALS:
+        remove_codepoints = dict.fromkeys(
+            INVALID_CODEPOINTS[target_xml_version])
+        if remove in (RESTRICTED, DISCOURAGED):
+            remove_codepoints.update(
+                dict.fromkeys(
+                    RESTRICTED_CODEPOINTS[target_xml_version]))
+            if remove == DISCOURAGED:
+                remove_codepoints.update(
+                    dict.fromkeys(DISCOURAGED_CODEPOINTS))
+           #
+        #
+        cleaned_up_source = source_text.translate(remove_codepoints)
+    else:
+        raise ValueError(
+            'remove must be None or one of {0!r}!'.format(
+                SUPPORTED_CHARACTER_REMOVALS))
+
     #
-    if remove_discouraged:
-        remove_codepoints.update(
-            dict.fromkeys(DISCOURAGED_CODEPOINTS))
-    #
-    return xml.sax.saxutils.escape(
-        source_text.translate(remove_codepoints))
+    return xml.sax.saxutils.escape(cleaned_up_source)
 
 
 # vim: fileencoding=utf-8 ts=4 sts=4 sw=4 autoindent expandtab syntax=python:
