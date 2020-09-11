1d5da1add1812d4ed116baf7a7f508dcd1964519
blackstream-x <rainer@blackstream.de>
2020-09-11 17:25:19 +0200

    Add the cli module (#57)

--- smallparts/__init__.py
+++ smallparts/__init__.py
@@ -6,4 +6,4 @@ smallparts - A collection of convenience modules for Python 3
 
 """
 
-__version__ = '0.6.5'
+__version__ = '0.7.0'
---
+++ tests/test_cli.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+test smallparts.sequences
+
+"""
+
+import unittest
+
+from smallparts import cli
+
+
+class TestSimple(unittest.TestCase):
+
+    """Test the module"""
+
+    def test_single_command(self):
+        """Single command call"""
+        ls_call = cli.Subprocess(['ls', '-1d'])
+        self.assertEqual(
+            ls_call.result.stdout,
+            b'.\n')
+        self.assertRaises(
+            cli.IllegalStateException,
+            ls_call.run)
+        self.assertEqual(
+            ls_call.repeat().result.stdout,
+            b'.\n')
+        self.assertEqual(
+            cli.Subprocess('ls -1d').result.stdout,
+            b'.\n')
+        self.assertRaises(
+            OSError,
+            cli.Subprocess,
+            ['non-existent-command'])
+        self.assertRaisesRegex(
+            ValueError,
+            '^Invalid command',
+            cli.Subprocess,
+            None)
+        self.assertRaisesRegex(
+            ValueError,
+            '^Please provide at least one command.',
+            cli.Subprocess)
+
+    def test_pipeline(self):
+        """Shell pipeline call"""
+        pipeline_call = cli.Subprocess(
+            ['ls', '-1d'],
+            ['tr', '.', 'x'],
+            ['tr', 'x', '-'],
+            ['tr', '-', 'u'])
+        self.assertEqual(
+            pipeline_call.result.stdout,
+            b'u\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+# vim:fileencoding=utf-8 autoindent ts=4 sw=4 sts=4 expandtab:
